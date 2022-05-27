9ce5e9fd476880f5fed2fa43be2b9faacf4be237
Vartic Mihai <mihai.vlad.vartic@gmail.com>
2022-05-27 23:30:35 +0300

    Added NLP to the chatbot

--- main.py
+++ main.py
@@ -11,6 +11,17 @@ import warnings
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from PIL import Image, ImageTk
+import warnings
+import pickle
+import nltk
+# nltk.download('punkt')
+from nltk.stem.lancaster import LancasterStemmer
+import numpy as np
+import tflearn
+import tensorflow as tf
+import random
+import json
+from tensorflow.python.framework import ops
 
 
 class GUI:
@@ -109,7 +120,7 @@ class GUI:
 
     def greet(self):
         self.textCons.config(state=DISABLED)
-        message = "Varothex: Hello there, my name is Varothex. I will answer your queries. If you want to exit, " \
+        message = "Varothex: Hello there, my name is Varothex. I'm here for you! If you want to exit, " \
                   "type „bye”! "
         self.textCons.config(state=NORMAL)
         self.textCons.insert(END, message + "\n\n")
@@ -130,7 +141,7 @@ class GUI:
 
     def send(self):
         self.textCons.config(state=DISABLED)
-        message = bot_response(self.msg)
+        message = "Varothex: " + response(self.msg)                                # afisam raspunsul
         self.textCons.config(state=NORMAL)
         self.textCons.insert(END, message + "\n\n")
         self.textCons.config(state=DISABLED)
@@ -138,81 +149,192 @@ class GUI:
 
 
 warnings.filterwarnings('ignore')
+stemmer = LancasterStemmer()
 
-f = open('C:\\Users\\mihai\\Desktop\\Serios\\Projects\\Python\\Bot\\robot.txt', 'r', errors='ignore')
-raw = f.read()
-raw = raw.lower()
+with open('intents.json') as json_data:
+    intents = json.load(json_data)
 
-sent_tokens = nltk.sent_tokenize(raw)  # converts to list of scentences
-word_tokens = nltk.word_tokenize(raw)  # converts to list of words
+words = []
+classes = []
+documents = []
+ignore_words = ['?', "'m", "'re", "'s", ')', ',', '.', ':']  # eventual aici putem defini o lista de STOPWORDS
 
-sentToken = sent_tokens[:4]
-wordToken = word_tokens[:4]
+# pentru fiecare fraza din sabloanele corespunzatoare unei intentii
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        # tokenizare
+        w = nltk.word_tokenize(pattern)
+        # adaugam cuvintele la o lista globala cu toate cuvintele din texte
+        words.extend(w)
+        # adaugam textul tokenizat la multimea de documente
+        documents.append((w, intent['tag']))
+        # adaugam clasa (tipul de intentie) la lista de clase existente
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
 
-# preprocessing
-lemmer = nltk.stem.WordNetLemmatizer()
+# aplicam stemming si lowercasing pentru fiecare cuvant intalnit, ignoram stopwords
+words = [stemmer.stem(w.lower()) for w in words if w not in ignore_words]
+words = sorted(list(set(words)))  # eliminam duplciate
+
+classes = sorted(list(set(classes)))
+
+# construim datele de antrenare
+training = []
+output = []
+# un vector de 0 de lungime egala cu numarul de clase (vom folosi reprezentarea one-hot a claselor)
+output_empty = [0] * len(classes)
+
+# bag of words pentru fiecare fraza
+for doc in documents:
+    # initializam bag of words
+    bag = []
+    # lista de tokeni pentru fraza curenta
+    pattern_words = doc[0]
+    # aplicam stemming
+    pattern_words = [stemmer.stem(word.lower()) for word in pattern_words]
+    # construim vectorul binar pentru bag of words
+    for w in words:
+        bag.append(1) if w in pattern_words else bag.append(0)
+
+    # output este '1' pentru tagul corespunzator frazei si '0' pentru celelalte (one-hot)
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+
+    training.append([bag, output_row])
+
+# shuffle + transformare in np.array
+random.shuffle(training)
+training = np.array(training)
+
+# feature-urile de antrenare
+train_x = list(training[:, 0])
+# etichete/labels (ce dorim sa returneze modelul)
+train_y = list(training[:, 1])
+
+ops.reset_default_graph()  # resetam starea engine-ului TensorFlow
+
+# definim reteaua
+net = tflearn.input_data(shape=[None, len(train_x[0])])  # toti vectorii de bag-of-words au aceasta dimensiune
+net = tflearn.fully_connected(net, 8)  # strat feed-forward ascuns cu 8 noduri
+net = tflearn.fully_connected(net, 8)  # strat feed-forward ascuns cu 8 noduri
+net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')  # numarul de noduri output = numarul de clase
+net = tflearn.regression(net)  # folosim acest strat de logistic regression pentru a extrage probabilitatile claselor
+
+# definim modelul final
+model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
+
+# incepem antrenarea folosind coborarea pe gradient
+# trecem prin model cate 8 fraze odata (batch size = 8)
+model.fit(train_x, train_y, n_epoch=1000, batch_size=8, show_metric=True)
+model.save('model.tflearn')
+
+import pickle
+pickle.dump({'words': words, 'classes': classes, 'train_x': train_x, 'train_y': train_y}, open("training_data", "wb"))
+
+data = pickle.load(open("training_data", "rb"))
+
+words = data['words']
+classes = data['classes']
+train_x = data['train_x']
+train_y = data['train_y']
+
+with open('intents.json') as json_data:
+    intents = json.load(json_data)
+
+# incarcam weight-urile salvate pentru cel mai bun model de clasificare
+model.load('model.tflearn')
 
 
-def LemTokens(tokens):
-    return [lemmer.lemmatize(token) for token in tokens]
+def clean_up_sentence(sentence):
+    # tokenizare
+    sentence_words = nltk.word_tokenize(sentence)
+    # stemming
+    sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
+    return sentence_words
 
 
-remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+# reprezentare binara bag-of-words
+def bow(sentence, words):
+    sentence_words = clean_up_sentence(sentence)
+    # bag of words
+    bag = [0] * len(words)
+    for s in sentence_words:
+        for i, w in enumerate(words):
+            if w == s:
+                bag[i] = 1
+
+    return np.array(bag)
 
 
-def LemNormalize(text):
-    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+ERROR_THRESHOLD = 0.25
 
 
-# Greetings
-GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up", "hey")
-GREETING_RESPONSES = ["Hi!", "Hey!", "*nods*", "Hello there!", "Hello!"]
+def classify(sentence):
+    # probabilitatile prezise de model
+    results = model.predict([bow(sentence, words)])[0]
+    # renuntam la intentiile cu probabilitate mica
+    results = [[i, r] for i, r in enumerate(results) if r > ERROR_THRESHOLD]
+    # sortam descrescator intentiile dupa probabilitate
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append((classes[r[0]], r[1]))
+    # return perechi (intentie, probabilitate)
+    return return_list
 
 
-def greeting(scentence):
-    for word in scentence.split():
-        if word.lower() in GREETING_INPUTS:
-            return random.choice(GREETING_RESPONSES)
+def response(sentence):
+    results = classify(sentence)
+    # daca avem macar o intentie valida, o procesam pe cea cu probabilitate maxima
+    if results:
+        while results:
+            for i in intents['intents']:
+                # cautam in dictionarul de intentii tagul returnat
+                if i['tag'] == results[0][0]:
+                    # returnam un raspuns aleator corespunzator intentiei
+                    return random.choice(i['responses'])
+
+            results.pop(0)
+            # daca nu am putut da un raspuns pentru aceasta intentie, trecem la urmatoarea cu probabilitate maxima
+
+    return "Sorry, I don't understand."  # nu a putut fi stabilita o intentie pentru
+    # fraza introdusa
 
 
-def response(user_response):
-    chatbot_response = ''
-    sent_tokens.append(user_response)
-    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words="english")
-    tfidf = TfidfVec.fit_transform(sent_tokens)
-    vals = cosine_similarity(tfidf[-1], tfidf)
-    idx = vals.argsort()[0][-2]
-    flat = vals.flatten()
-    flat.sort()
-    req_tfidf = flat[-2]
-    if req_tfidf == 0:
-        chatbot_response = chatbot_response + "I am sorry, I didn't understand you. :("
-        return chatbot_response
-
-    else:
-        chatbot_response = chatbot_response + sent_tokens[idx]
-        return chatbot_response
+# structura de date pentru context
+context = {}
 
 
-def bot_response(user_response):
-    user_response = user_response.lower()
-    if user_response != 'bye':
-        if user_response.lower() == 'thanks' or user_response == 'thank you':
-            # print("Varothex: You're welcome!")
-            return "Varothex: You're welcome!"
-        else:
-            if greeting(user_response) is not None:
-                # print("Varothex: " + greeting(user_response))
-                return "Varothex: " + greeting(user_response)
-            else:
-                # print("Varothex: ", end='')
-                # print(response(user_response))
-                # sent_tokens.remove(user_response)
-                return "Varothex: " + response(user_response)
-                # sent_tokens.remove(user_response)
-    else:
-        # print("Varothex: Bye!")
-        return "Varothex: Bye!"
+# retinem pentru un user contextul curent, in functie de un ID specific
+def response(sentence, userID='123', show_details=False):
+    results = classify(sentence)
+    # daca avem macar o intentie valida, o procesam pe cea cu probabilitate maxima
+    if results:
+        # loop as long as there are matches to process
+        while results:
+            for i in intents['intents']:
+                # cautam in dictionarul de intentii tagul returnat
+                if i['tag'] == results[0][0]:
+                    # daca intentia curenta asteapta un context pentru a fi valida, verificam ca contextul actual sa
+                    # fie indeplinit
+                    if ('context_filter' not in i
+                            or (userID in context and 'context_filter' in i and i['context_filter'] == context[
+                                userID])):
+                        if show_details:
+                            print('tag:', i['tag'])
+                        # daca intentia curenta actualizeaza contextul
+                        if 'context_set' in i:
+                            if show_details:
+                                print('context:', i['context_set'])
+                            context[userID] = i['context_set']
+                        # returnam un raspuns aleator corespunzator intentiei
+                        return random.choice(i['responses'])
+            results.pop(0)
+            # daca nu am putut da un raspuns pentru aceasta intentie, trecem la urmatoarea cu probabilitate maxima
+    # else:
+        # folosim vechiul cod
+
+    return "Sorry, I wasn't trained about this subject."  # nu a putut fi stabilita o intentie pentru fraza introdusa
 
 
 if __name__ == "__main__":
