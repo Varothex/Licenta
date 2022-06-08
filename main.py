a2f126e155a98b18cca9fadee8827fd6b03cd126
Vartic Mihai <mihai.vlad.vartic@gmail.com>
2022-06-08 03:33:41 +0300

    Added animations, cleaned the code

--- main.py
+++ main.py
@@ -70,15 +70,13 @@ class GUI:
         self.Window.title("Varothex")
         self.Window.resizable(width=False, height=False)
         self.Window.configure(width=1200, height=700, bg="#17202A")
+
         width = 0.7
 
         # avatar
-        self.avatarFrame = Frame(self.Window)
-        self.avatarFrame.pack()
+        self.avatar = PhotoImage(file='avatar.png')  # need a reference to the image or it gets garbage collected
+        self.avatarFrame = Label(self.Window, image=self.avatar)
         self.avatarFrame.place(relx=width, rely=0)
-        avatar = PhotoImage(file='avatar.png')
-        self.avatar = avatar  # You always need a reference to the image or it gets garbage collected
-        Label(self.avatarFrame, image=avatar).grid()
 
         # chat window
         self.chatWindow = Text(self.Window, width=20, height=2, bg="#17202A", fg="#EAECEE", font="Roboto 14", padx=5,
@@ -102,20 +100,12 @@ class GUI:
 
         # send button
         self.buttonMsg = Button(self.labelBottom, text="Send", font="Roboto 14 bold", width=20, bg="#ABB2B9",
-                                command=lambda: self.sendButton(self.textbox.get()))
-        self.Window.bind('<Return>', lambda event: self.sendButton(self.textbox.get()))
+                                command=lambda: self.sendMessage(self.textbox.get()))
+        self.Window.bind('<Return>', lambda event: self.sendMessage(self.textbox.get()))
         self.buttonMsg.place(relx=0.745, rely=0.015, relheight=0.06, relwidth=0.22)
         self.chatWindow.config(cursor="arrow")
 
         self.greet()
-
-    # function to basically start the thread for sending messages
-    def sendButton(self, msg):
-        self.chatWindow.config(state=DISABLED)
-        self.msg = msg
-        self.textbox.delete(0, END)
-        snd = threading.Thread(target=self.sendMessage)
-        snd.start()
 
     def greet(self):
         self.chatWindow.config(state=DISABLED)
@@ -132,8 +122,16 @@ class GUI:
         self.chatWindow.config(state=DISABLED)
         self.chatWindow.see(END)
 
-    # function to send messages
-    def sendMessage(self):
+    # function to submit the messages
+    def sendMessage(self, msg):
+        self.chatWindow.config(state=DISABLED)
+        self.msg = msg
+        self.textbox.delete(0, END)
+        snd = threading.Thread(target=self.showMessage)
+        snd.start()
+
+    # function to print the messages
+    def showMessage(self):
         self.chatWindow.config(state=DISABLED)
         while True:
             time = strftime('%H:%M')
@@ -145,20 +143,30 @@ class GUI:
             self.botAnswer()
             break
 
-    # return the bot answer
+    # function to return the bot answer
     def botAnswer(self):
         self.chatWindow.config(state=DISABLED)
         time = strftime('%H:%M')
-        message = time + " Varothex: " + response(self.msg)
+        message, feeling = response(self.msg)
+        message = time + " Varothex: " + message
         self.chatWindow.config(state=NORMAL)
         self.chatWindow.insert(END, message + "\n\n")
         self.chatWindow.config(state=DISABLED)
         self.chatWindow.see(END)
 
-        # TODO animation
-        # avatar = PhotoImage(file='avatar_happy.png')
-        # self.avatar = avatar
-        # Label(self.avatarFrame, image=avatar).grid()
+        # animations
+        if feeling == 'happy':
+            self.avatar = PhotoImage(file='avatar_happy.png')
+            self.avatarFrame.configure(image=self.avatar)
+            self.avatarFrame.image = self.avatar
+        elif feeling == 'sad':
+            self.avatar = PhotoImage(file='avatar_sad.png')
+            self.avatarFrame.configure(image=self.avatar)
+            self.avatarFrame.image = self.avatar
+        else:
+            self.avatar = PhotoImage(file='avatar.png')
+            self.avatarFrame.configure(image=self.avatar)
+            self.avatarFrame.image = self.avatar
 
 
 # TODO mic input
@@ -181,107 +189,90 @@ stemmer = LancasterStemmer()
 with open('intents.json') as json_data:
     intents = json.load(json_data)
 
+# tokenizing words
 words = []
 classes = []
 documents = []
-ignore_words = ['?', "'m", "'re", "'s", ',', '.', ':']  # STOPWORDS
+ignore_words = ['?', "'", ',', '.', '!']  # STOPWORDS
 
-# pentru fiecare fraza din sabloanele corespunzatoare unei intentii
 for intent in intents['intents']:
     for pattern in intent['patterns']:
-        # tokenizare
         w = nltk.word_tokenize(pattern)
-        # adaugam cuvintele la o lista globala cu toate cuvintele din texte
-        words.extend(w)
-        # adaugam textul tokenizat la multimea de documente
-        documents.append((w, intent['tag']))
-        # adaugam clasa (tipul de intentie) la lista de clase existente
-        if intent['tag'] not in classes:
+        words.extend(w)  # adaugam cuvintele la o lista globala cu toate cuvintele din texte
+        documents.append((w, intent['tag']))  # adaugam textul tokenizat la multimea de documente
+        if intent['tag'] not in classes:  # adaugam clasa (tipul de intentie) la lista de clase existente
             classes.append(intent['tag'])
 
 # aplicam stemming si lowercasing pentru fiecare cuvant intalnit, ignoram stopwords
 words = [stemmer.stem(w.lower()) for w in words if w not in ignore_words]
 words = sorted(list(set(words)))  # eliminam duplciate
-
 classes = sorted(list(set(classes)))
 
 # construim datele de antrenare
 training = []
 output = []
-# un vector de 0 de lungime egala cu numarul de clase (vom folosi reprezentarea one-hot a claselor)
-output_empty = [0] * len(classes)
+output_empty = [0] * len(
+    classes)  # un vector de 0 de lungime egala cu numarul de clase (vom folosi reprezentarea one-hot a claselor)
 
-# bag of words pentru fiecare fraza
-for doc in documents:
-    # initializam bag of words
+for doc in documents:  # bag of words pentru fiecare fraza
     bag = []
-    # lista de tokeni pentru fraza curenta
-    pattern_words = doc[0]
-    # aplicam stemming
+
+    pattern_words = doc[0]  # lista de tokeni pentru fraza curenta
     pattern_words = [stemmer.stem(word.lower()) for word in pattern_words]
-    # construim vectorul binar pentru bag of words
-    for w in words:
+
+    for w in words:  # construim vectorul binar pentru bag of words
         bag.append(1) if w in pattern_words else bag.append(0)
 
-    # output este '1' pentru tagul corespunzator frazei si '0' pentru celelalte (one-hot)
     output_row = list(output_empty)
-    output_row[classes.index(doc[1])] = 1
+    output_row[classes.index(
+        doc[1])] = 1  # output este '1' pentru tagul corespunzator frazei si '0' pentru celelalte (one-hot)
 
     training.append([bag, output_row])
 
-# shuffle + transformare in np.array
 random.shuffle(training)
 training = np.array(training)
 
-# feature-urile de antrenare
-train_x = list(training[:, 0])
-# etichete/labels (ce dorim sa returneze modelul)
-train_y = list(training[:, 1])
+train_x = list(training[:, 0])  # feature-urile de antrenare
+train_y = list(training[:, 1])  # etichete/labels (ce dorim sa returneze modelul)
 
+# creating model and training
 ops.reset_default_graph()  # resetam starea engine-ului TensorFlow
 
-# definim reteaua
 net = tflearn.input_data(shape=[None, len(train_x[0])])  # toti vectorii de bag-of-words au aceasta dimensiune
-net = tflearn.fully_connected(net, 8)  # strat feed-forward ascuns cu 8 noduri
+net = tflearn.fully_connected(net, 64)  # , activation='ReLu'
+net = tflearn.fully_connected(net, 32)
 net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')  # numarul de noduri output = numarul de clase
 net = tflearn.regression(net)  # folosim acest strat de logistic regression pentru a extrage probabilitatile claselor
 
-# definim modelul final
-model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
-
-
-def training():
-    # incepem antrenarea folosind coborarea pe gradient, trecem prin model cate 8 fraze odata (batch size)
-    model.fit(train_x, train_y, n_epoch=2000, batch_size=8, show_metric=True)
-    model.save('model.tflearn')
-
+model = tflearn.DNN(net, tensorboard_dir='tflearn_logs', tensorboard_verbose=3)  # definim modelul final
+model.fit(train_x, train_y, n_epoch=100, batch_size=8,
+          show_metric=True)  # incepem antrenarea folosind coborarea pe gradient, trecem prin model cate 8 fraze odata
+model.save('model.tflearn')
 
 pickle.dump({'words': words, 'classes': classes, 'train_x': train_x, 'train_y': train_y}, open("training_data", "wb"))
 
+# testing
 data = pickle.load(open("training_data", "rb"))
-
 words = data['words']
 classes = data['classes']
 train_x = data['train_x']
 train_y = data['train_y']
 
-# incarcam weight-urile salvate pentru cel mai bun model de clasificare
-model.load('model.tflearn')
+model.load('model.tflearn')  # incarcam weight-urile salvate pentru cel mai bun model de clasificare
 
 
 def clean_up_sentence(sentence):
-    # tokenizare
-    sentence_words = nltk.word_tokenize(sentence)
-    # stemming
-    sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
+    sentence_words = nltk.word_tokenize(sentence)  # tokenizare
+    sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]  # stemming
+
     return sentence_words
 
 
 # reprezentare binara bag-of-words
 def bow(sentence, words):
     sentence_words = clean_up_sentence(sentence)
-    # bag of words
     bag = [0] * len(words)
+
     for s in sentence_words:
         for i, w in enumerate(words):
             if w == s:
@@ -290,41 +281,38 @@ def bow(sentence, words):
     return np.array(bag)
 
 
-ERROR_THRESHOLD = 0.6
+ERROR_THRESHOLD = 0.4
 
 
 def classify(sentence):
-    # probabilitatile prezise de model
-    results = model.predict([bow(sentence, words)])[0]
-    # renuntam la intentiile cu probabilitate mica
-    results = [[i, r] for i, r in enumerate(results) if r > ERROR_THRESHOLD]
-    # sortam descrescator intentiile dupa probabilitate
-    results.sort(key=lambda x: x[1], reverse=True)
+    results = model.predict([bow(sentence, words)])[0]  # probabilitatile prezise de model
+    results = [[i, r] for i, r in enumerate(results) if r > ERROR_THRESHOLD]  # renuntam la intentiile improbabile
+    results.sort(key=lambda x: x[1], reverse=True)  # sortam descrescator intentiile dupa probabilitate
     return_list = []
+
     for r in results:
         return_list.append((classes[r[0]], r[1]))
-    # return perechi (intentie, probabilitate)
-    return return_list
+
+    return return_list  # return perechi (intentie, probabilitate)
 
 
 # structura de date pentru context
 context = {}
 
 
-def response(sentence, userID='123'):
+def response(sentence, userID='27'):
     results = classify(sentence)
 
-    # daca avem macar o intentie valida, o procesam pe cea cu probabilitate maxima
-    if results:
+    if results:  # daca avem macar o intentie valida, o procesam pe cea cu probabilitate maxima
         while results:
             for i in intents['intents']:
-                # cautam in dictionarul de intentii tagul returnat
-                if i['tag'] == results[0][0]:
+                if i['tag'] == results[0][0]:  # cautam in dictionarul de intentii tagul returnat
                     if 'context_filter' not in i or (userID in context and 'context_filter' in i and i['context_filter']
                                                      == context[userID]):
-                        # daca intentia curenta actualizeaza contextul
-                        if 'context_set' in i:
+                        if 'context_set' in i:  # daca intentia curenta actualizeaza contextul
                             context[userID] = i['context_set']
+                        else:
+                            context[userID] = {}
 
                         if i['tag'] == "weather":
                             html = weatherAcces().content
@@ -336,27 +324,30 @@ def response(sentence, userID='123'):
                             tts.save('tts/' + ttsResponse)
                             mixer.music.load('tts/' + ttsResponse)
                             mixer.music.play()
-                            return submission_count_text
+                            return submission_count_text, 'neutral'
 
-                        # returnam un raspuns aleator corespunzator intentiei
-                        botResponse = random.choice(i['responses'])
+                        botResponse = random.choice(i['responses'])  # returnam un raspuns corespunzator intentiei
                         tts = gTTS(text=botResponse, lang='en', slow=False)
                         date_string = datetime.now().strftime("%d%m%Y%H%M%S")
                         ttsResponse = "tts." + date_string + ".mp3"
                         tts.save('tts/' + ttsResponse)
                         mixer.music.load('tts/' + ttsResponse)
                         mixer.music.play()
-                        return botResponse
+                        if i['tag'] == "greetingGood" or "compliment" or "feelingGood" or "approveJoke" or \
+                                "joke" or "funny" or "thanks" or "goodbye":
+                            return botResponse, 'happy'
+                        elif i['tag'] == "empty" or "greetingBad" or "feelingBad" or "disapproveJoke":
+                            return botResponse, 'sad'
+                        return botResponse, 'neutral'
 
-            results.pop(0)
-            # daca nu am putut da un raspuns pentru aceasta intentie, trecem la urmatoarea cu probabilitate maxima
+            results.pop(0)  # daca nu am putut da un raspuns, trecem la urmatoarea intentie cu probabilitate maxima
 
     # myobj = gTTS(text="Sorry, I can't understand you. :(", lang='en', slow=False)
     # myobj.save("notUnderstand.mp3")
     notUnderstand = 'notUnderstand.mp3'
     mixer.music.load(notUnderstand)
     mixer.music.play()
-    return "Sorry, I can't understand you. :("  # nu a putut fi stabilita o intentie pentru fraza introdusa
+    return "Sorry, I can't understand you. :(", 'sad'  # nu a putut fi stabilita o intentie pentru fraza introdusa
 
 
 def weatherAcces():
@@ -365,8 +356,6 @@ def weatherAcces():
 
 if __name__ == "__main__":
     # text = get_audio()
-
-    # training()
 
     # we remove any previous tts files
     directory = 'tts/'
